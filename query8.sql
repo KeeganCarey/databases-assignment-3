9f0d93272e33679613175a1975987bda38a72389
KeeganCarey <keegancarey12@gmail.com>
2026-02-11 22:42:26 -0800

    query8 done

--- query8.sql
+++ query8.sql
@@ -1,7 +1,15 @@
-SELECT genres.Name AS Genre, SUM(tracks.Milliseconds) AS GenreLength
-FROM genres
-JOIN tracks ON genres.GenreId = tracks.GenreId
-GROUP BY genres.Name
+SELECT tracks.Name, tracks.Composer, tracks.Milliseconds
+FROM tracks
 
-/*sort it*/
-ORDER BY GenreLength DESC;
+/*make the condition be for it to NOT be in the table of
+the top 5 longest genres in total*/ 
+WHERE GenreId NOT IN (
+    SELECT genres.GenreId
+    FROM genres
+    JOIN tracks ON genres.GenreId = tracks.GenreId
+    GROUP BY genres.GenreId
+    ORDER BY SUM(tracks.Milliseconds) DESC
+    LIMIT 5
+)
+/*i wanted to sort it alphabetically */
+ORDER BY tracks.Name;
